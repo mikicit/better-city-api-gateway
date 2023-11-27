9aa48ac5eb2a9364df86fee4ccd88ec96d25a640
Mikita Citarovic <titorovichnikita@gmail.com>
2023-11-27 04:27:56 +0100

    project init

---
+++ src/main/java/dev/mikita/apigateway/ApiGatewayApplication.java
@@ -0,0 +1,20 @@
+package dev.mikita.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * The type Api gateway application.
+ */
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+	/**
+	 * The entry point of application.
+	 *
+	 * @param args the input arguments
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+}
---
+++ src/test/java/dev/mikita/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,19 @@
+package dev.mikita.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * The type Api gateway application tests.
+ */
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+    /**
+     * Context loads.
+     */
+    @Test
+	void contextLoads() {
+	}
+
+}
